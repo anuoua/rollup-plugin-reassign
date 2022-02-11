f2da277b307dbda4c2ee1d91dd6f19c6f6fdd1a1
anuoua <anuoua@gmail.com>
2022-02-11 14:22:04 +0800

    chore: dist change

--- rollup.config.js
+++ rollup.config.js
@@ -4,12 +4,18 @@ import { defineConfig } from "rollup";
 const configGen = (format) =>
   defineConfig({
     input: "src/index.ts",
-    plugins: [typescript({ include: "src/**/*.ts", module: "esnext" })],
+    plugins: [
+      typescript({
+        include: "src/**/*.ts",
+        module: "esnext",
+        declarationDir: `dist/${format}`,
+      }),
+    ],
     external: ["@rollup/pluginutils", "estree-walker", "magic-string"],
     output: {
       sourcemap: true,
-      dir: "dist",
-      entryFileNames: `index.${format}.js`,
+      dir: `dist/${format}`,
+      entryFileNames: `index.js`,
       format,
     },
   });
